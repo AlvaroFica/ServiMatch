94a782f51eeddc4094f602d0711f777e82fd6389
Javier Diaz <jav.diazd@duocuc.cl>
2025-05-04 23:51:55 -0400

    Se arreglaron problemas

--- backend/principal/settings.py
+++ backend/principal/settings.py
@@ -3,7 +3,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -69,9 +70,10 @@ WSGI_APPLICATION = 'principal.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  # <- esto está bien
     }
 }
+
 
 
 # Password validation
