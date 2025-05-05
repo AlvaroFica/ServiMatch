ae27cf8c11d43b1ec164b5f2bb656b37c288bfbb
Javier Diaz <jav.diazd@duocuc.cl>
2025-05-04 23:28:41 -0400

    Se crean los routers

--- backend/usuarios/urls.py
+++ backend/usuarios/urls.py
@@ -1,7 +1,26 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import *
+
+router = DefaultRouter()
+router.register(r'paises', PaisViewSet)
+router.register(r'regiones', RegionViewSet)
+router.register(r'ciudades', CiudadViewSet)
+router.register(r'comunas', ComunaViewSet)
+router.register(r'usuarios', UsuarioViewSet)
+router.register(r'membresias', MembresiaViewSet)
+router.register(r'especialidades', EspecialidadViewSet)
+router.register(r'tipoespecialidades', TipoEspecialidadViewSet)
+router.register(r'trabajadores', TrabajadorViewSet)
+router.register(r'tiposervicios', TipoServicioViewSet)
+router.register(r'servicios', ServicioViewSet)
+router.register(r'tipopagos', TipoPagoViewSet)
+router.register(r'boletas', BoletaViewSet)
+router.register(r'pagos', PagoViewSet)
+router.register(r'citas', CitaViewSet)
+router.register(r'chats', ChatViewSet)
+router.register(r'mensajes', MensajeViewSet)
 
 urlpatterns = [
-    path('login/', views.vista_login, name='login'),
-    path('registrar-trabajador/', views.vista_registrar_trabajador, name='Registrar-trabajador')
+    path('', include(router.urls)),
 ]
